0072c6ba92573540f5d4ab24f3e18358411c7d3a
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-24 12:15:44 +0600

    created fileDb.ts; added init, getNews, addOneNews, getOneNewsById, deleteOneNews, save methods;

---
+++ api/fileDb.ts
@@ -0,0 +1,51 @@
+import {promises as fs} from 'fs';
+import {Comment, OneNews, OneNewsType} from "./types";
+
+const fileName = './db.json';
+let data = {
+    news: [] as OneNews[],
+    comments: [] as Comment[],
+};
+
+const fileDb = {
+    async init() {
+        try {
+            const fileContents = await fs.readFile(fileName);
+            data = JSON.parse(fileContents.toString());
+        } catch (error) {
+            data = {
+                news: [],
+                comments: []
+            };
+        }
+    },
+    async getNews() {
+        return data.news.map(oneNews => ({
+            id: oneNews.id,
+            title: oneNews.title,
+            image: oneNews.image,
+            created_at: oneNews.created_at,
+        }));
+    },
+    async addOneNews(oneNews: OneNewsType) {
+        const id = crypto.randomUUID();
+        const created_at = new Date().toISOString();
+        const oneNewsData = {id, ...oneNews, created_at};
+        data.news.push(oneNewsData);
+        await this.save();
+        return oneNewsData;
+    },
+    async getOneNewsById(id: string) {
+        const oneNews = data.news.find(oneNews => oneNews.id === id);
+        return oneNews || null;
+    },
+    async deleteOneNews(id: string) {
+        data.news = data.news.filter(oneNews => oneNews.id !== id);
+        await this.save();
+    },
+    async save() {
+        return fs.writeFile(fileName, JSON.stringify(data, null, 2));
+    }
+};
+
+export default fileDb;
