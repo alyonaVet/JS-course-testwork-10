554fa382da6f8ce5860063cf08b964f46e471d26
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-24 19:09:54 +0600

    added static;

--- api/index.ts
+++ api/index.ts
@@ -19,6 +19,7 @@ const corsOptions: CorsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(express.static('public'));
 app.use('/news', newsRouter);
 app.use('/comments', commentsRouter);
 
