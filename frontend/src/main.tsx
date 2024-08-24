df2a300d2376a785f125d96b44be106e33ee61ac
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-24 14:44:28 +0600

    added Router, Provers store;

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,15 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {store} from './app/store';
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
+  <StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
         <App/>
-    </StrictMode>,
+      </Provider> </BrowserRouter>
+  </StrictMode>,
 );
