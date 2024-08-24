f92ecfd79655b1a99777e2ebe00393f71227f356
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-24 11:27:32 +0600

    added routers;

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors, {CorsOptions} from 'cors';
+import newsRouter from "./routers/news";
+import commentsRouter from "./routers/comments";
 
 const app = express();
 const port = 8000;
@@ -16,6 +18,9 @@ const corsOptions: CorsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use('/news', newsRouter);
+app.use('/comments', commentsRouter);
+
 
 const run = async () => {
 
