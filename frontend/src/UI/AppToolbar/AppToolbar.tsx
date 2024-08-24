34fcf55b5a90baef9c75f124e577266fdbecc26f
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-24 19:11:56 +0600

    refactor AppToolbar position;

--- frontend/src/UI/AppToolbar/AppToolbar.tsx
+++ frontend/src/UI/AppToolbar/AppToolbar.tsx
@@ -4,7 +4,7 @@ import {AppBar, Box, Toolbar, Typography} from '@mui/material';
 const AppToolbar = () => {
   return (
     <Box sx={{flexGrow: 1}}>
-      <AppBar position="fixed" sx={{mb: 2}}>
+      <AppBar position="static" sx={{mb: 2}}>
         <Toolbar>
           <NavLink to="/" style={{textDecoration: 'none', color: 'inherit'}}>
             <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
