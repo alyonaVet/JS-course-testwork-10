2d9e42f661c621d187b57be5378e4ac61781bd2a
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-24 19:15:29 +0600

    added Routers;

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
+import AppToolbar from './UI/AppToolbar/AppToolbar';
+import {Route, Routes} from 'react-router-dom';
+import MainPage from './features/Pages/MainPage';
+import AddPostPage from './features/AddPostPage/AddPostPage';
+import OneNewsPage from './features/Pages/OneNewsPage';
+
 const App = () => {
 
-    return (
-        <>
-            Hello World!
-        </>
-    )
+  return (
+    <>
+      <header>
+        <AppToolbar/>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/new-post" element={<AddPostPage />} />
+          <Route path="/news/:newsId" element={<OneNewsPage />} />
+
+        </Routes>
+      </main>
+    </>
+  );
 };
 
 export default App;
